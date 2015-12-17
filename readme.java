faa24a2292f5e084ad9d21261f88604260a5f0b1
mopeng <364586903@qq.com>
2015-12-17 20:23:20 +0800

    dev分支上增加内容

--- readme.java
+++ readme.java
@@ -2,4 +2,7 @@ public class Test{
       public static void main(String args[]){
           System.out.println("ÄãºÃ");
       }
+      public void createStr(){
+      	System.out.println("str hello")
+      }
 }
